d8b2f5395f3fd5b6d149a5f0909d88ba9f3ba1db
ChrisAchinga <achinga.chris@gmail.com>
2020-09-09 20:38:32 +0300

    lint

--- components/homepage/CategoryBanner.js
+++ components/homepage/CategoryBanner.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Link from 'next/link';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
+import { connect } from "react-redux";
 
 class CategoryBanner extends Component {
   render() {
@@ -16,24 +16,20 @@ class CategoryBanner extends Component {
 
           <div className="row">
             {categories.map((item, index) => (
-              <div
-              key={`category-item-${index}`}
-              className="col-12 col-md-4 collection-item mb-5"
-            >
+              <div key={`category-item-${index}`} className="col-12 col-md-4 collection-item mb-5">
                 <Link href={item.link}>
-                <a className="align-items-center font-color-black flex-column cursor-pointer mb-5">
-                <div>
-
-                    <div
-                      className="mb-4 w-100 collection-item-image"
-                      style={{
-                        background: `url("${item.image}") center center/cover`
-                      }}
-                    />
-                    <p className="mb-2 font-size-heading text-center" key={item.id}>
-                    {item.name}
-                    </p>
-                    <p className="text-center">{item.count} products</p>
+                  <a className="align-items-center font-color-black flex-column cursor-pointer mb-5">
+                    <div>
+                      <div
+                        className="mb-4 w-100 collection-item-image"
+                        style={{
+                          background: `url("${item.image}") center center/cover`,
+                        }}
+                      />
+                      <p className="mb-2 font-size-heading text-center" key={item.id}>
+                        {item.name}
+                      </p>
+                      <p className="text-center">{item.count} products</p>
                     </div>
                   </a>
                 </Link>
@@ -54,4 +50,4 @@ CategoryBanner.defaultProps = {
   categories: [],
 };
 
-export default connect(state => state)(CategoryBanner);
+export default connect((state) => state)(CategoryBanner);
